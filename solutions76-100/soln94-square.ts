7ae4dc4d60418c9c226265c19c471f95c5b0634c
vincent <100250184+vguzman812@users.noreply.github.com>
2023-07-23 09:21:43 -0400

    Add solution 94

---
+++ solutions76-100/soln94-square.ts
@@ -0,0 +1,29 @@
+/**
+ * https://www.codewars.com/kata/523b623152af8a30c6000027/train/typescript
+ * @description
+ * Write a funciton that takes in a number and returns that number squared
+ * 
+ * @param {number} n
+ * @returns {number}
+ * @example
+ * Input: 2
+ * Output: 4
+ * 
+ * Input: 5
+ * Output: 25
+ * 
+ * Input: -3
+ * Output: 9
+ * 
+ * Input: -2.2
+ * Output: 4.84
+ * 
+ * 
+ * @pseudocode
+ * Square the number using ** operator
+ * return the squared  number
+ */
+
+function square(n): number{
+    return n**2
+}
