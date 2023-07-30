a5553fb8b1c914ae5a6b31b061a29f31c012843c
vincent <100250184+vguzman812@users.noreply.github.com>
2023-07-30 18:38:15 -0400

    Add solutions 55

---
+++ solutions51-75/soln55-stringToNum.js
@@ -0,0 +1,10 @@
+// // We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+const stringToNumber = function(str){
+    return Number(str)
+  }
