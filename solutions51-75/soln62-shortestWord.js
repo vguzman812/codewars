4525ed00627006b801ae06a6115ed159e77d4192
vincent <100250184+vguzman812@users.noreply.github.com>
2023-08-02 08:29:13 -0400

    Add solution 62

---
+++ solutions51-75/soln62-shortestWord.js
@@ -0,0 +1,8 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+function findShort(s){
+    return s.split(" ").sort((a, b) => a.length - b.length)[0].length
+}
+
+// Math.min(...s.split(" ").map(w => w.length))
