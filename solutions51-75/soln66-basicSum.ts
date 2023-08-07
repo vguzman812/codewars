8db256f24fde88330c3e539b2dd93348bec4cbee
vincent <100250184+vguzman812@users.noreply.github.com>
2023-08-07 17:19:28 -0400

    Finish first typescript problem solution 66

---
+++ solutions51-75/soln66-basicSum.ts
@@ -0,0 +1,6 @@
+// Complete the function to return his total number of goals in all three leagues.
+// 5, 10, 2  -->  17
+
+export function goals (laLigaGoals:number, copaDelReyGoals:number, championsLeagueGoals:number): number { 
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
