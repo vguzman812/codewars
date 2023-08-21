e8059fa7426dcdade8517744e6ec0c9e6a2518ea
vincent <100250184+vguzman812@users.noreply.github.com>
2023-08-21 15:21:31 -0400

    Add solution 77

---
+++ solutions76-100/soln77-doubleChar.ts
@@ -0,0 +1,14 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+export function doubleChar(str: string): string{
+   let newStr: string = "";
+    for (let i = 0; i < str.length; i++){
+        newStr += str[i].repeat(2);
+    }
+    return newStr
+  }
