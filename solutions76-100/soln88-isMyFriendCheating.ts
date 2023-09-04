5acb29dbf4ff88d6b83c29b3275a7b929e812645
vincent <100250184+vguzman812@users.noreply.github.com>
2023-09-03 20:20:16 -0400

    Add solution to solution 88

--- solutions76-100/soln88-isMyFriendCheating.ts
+++ solutions76-100/soln88-isMyFriendCheating.ts
@@ -6,7 +6,7 @@
  * He says that the product of a and b 
  * should be equal to the sum of all numbers in the sequence, excluding a and b.
  * Given a number n, 
- * could you tell me the numbers he excluded from the sequence?
+ * could you tell me the numbers he excluded from the sequence? (a and b)
  * The function takes the parameter: n (n is always strictly greater than 0) and
  * returns an array or a string (depending on the language) of the form:
  * [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or or [{a, b}, ...]
@@ -28,4 +28,28 @@
  * 
  * Input: 101
  * Output: [[55,91],[91,55]]
+ * 
+ * @pseudocode
+ *  initialize returning array
+ *  find sum of range n
+ *  so if n === 3
+ *      sum = 1 + 2 + 3 = 6
+ *  I could do n log n time complexity where I iterate through array with two for loops
+ *      if right value * left value === sum - right value - left value
+ *          push [left, right], [right, left] onto arr
+ *  return arr
  */
+
+export function removeNb (n:number):number[][] {
+    if (n === 1) return [[1,1], [1,1]]
+    let arr: number[][] = []
+    const sum = (n * (n + 1)) / 2; // sum of 1 to n
+    for (let i = 1; i < n; i++){
+        const j = (sum - i) / (i + 1)
+        if (j <= n && Number.isInteger(j)) {
+            arr.push([i, j]);
+          }
+    }
+    return arr
+  }
+
